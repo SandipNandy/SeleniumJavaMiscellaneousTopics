9bb3e96128277d3c8671c11bcdb17d25be74bfda
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-26 13:49:49 +0530

    initial commit
    Screenshot concepts is shown

---
+++ Screenshot.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
+
+public class Screenshot {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.youtube.com/");
+
+		Thread.sleep(6000);
+		File pic=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(pic, new File("..\\Pictures\\Screenshots\\seleniumpic.png"));
+		
+
+	}
+
+}
