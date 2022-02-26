a41e7c0e669b0f25076c53bda90a523db60b9185
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-26 13:43:36 +0530

    2nd commit
    cookies directory removed

--- DeleteCookies.java
+++ DeleteCookies.java
@@ -5,7 +5,7 @@ public class DeleteCookies {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
