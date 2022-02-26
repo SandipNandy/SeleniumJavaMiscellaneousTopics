ba1ca7b4719bd700ac4ba97f5d16697a639726ef
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-26 13:42:09 +0530

    initial commit
    Cookies deletion concepts

---
+++ DeleteCookies.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DeleteCookies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		//If you want to delete the cookies name wise then use below code
+		//driver.manage().deleteCookieNamed("ASF");
+
+	}
+
+}
