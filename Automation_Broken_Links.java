516750aab427d81051f8750457a7624793160dd5
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-27 13:26:08 +0530

    Initial Commit
    Check whether status of all links in the entire page

---
+++ Automation_Broken_Links.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automation_Broken_Links {
+
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		// TODO Auto-generated method stubs
+		// https://demoqa.com/links
+
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		String homePage = "http://www.zlti.com";
+		String url = "";
+		HttpURLConnection huc = null;
+		int respCode = 200;
+		driver.get(homePage);
+
+		driver.manage().timeouts().implicitlyWait(6000, TimeUnit.SECONDS);
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		java.util.Iterator<WebElement> it = links.iterator();
+
+		while (it.hasNext()) {
+
+			url = it.next().getAttribute("href");
+
+			System.out.println(url);
+
+			if (url == null || url.isEmpty()) {
+				System.out.println("URL is either not configured for anchor tag or it is empty");
+				continue;
+			}
+
+			if (!url.startsWith(homePage)) {
+				System.out.println("URL belongs to another domain, skipping it.");
+				continue;
+			}
+
+			try {
+				huc = (HttpURLConnection) (new URL(url).openConnection());
+
+				huc.setRequestMethod("HEAD");
+
+				huc.connect();
+
+				respCode = huc.getResponseCode();
+
+				if (respCode >= 400) {
+					System.out.println(url + " is a broken link");
+				} else {
+					System.out.println(url + " is a valid link");
+				}
+
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		driver.quit();
+
+	}
+
+}
