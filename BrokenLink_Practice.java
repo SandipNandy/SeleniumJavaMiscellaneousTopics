51854c5b1bc8f822ec1dff337278f0e5efbc91d2
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-28 21:44:17 +0530

    2nd Updates
    Dir as been changed

--- BrokenLink_Practice.java
+++ BrokenLink_Practice.java
@@ -13,7 +13,7 @@ public class BrokenLink_Practice {
 
 	public static void main(String[] args) throws MalformedURLException, IOException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
