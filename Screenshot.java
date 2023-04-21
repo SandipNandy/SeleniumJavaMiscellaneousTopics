eeac95dde9d9a57709f58bd3da390b48c8e7d8d8
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-21 08:45:08 +0530

    Update Screenshot.java

--- Screenshot.java
+++ Screenshot.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.apache.commons.io.FileUtils;
 
 public class Screenshot {
+	
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		System.setProperty("webdriver.chrome.driver",".\\chromedriver_win32\\chromedriver.exe");
