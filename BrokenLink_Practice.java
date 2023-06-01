0a1b5666a754ced8d47f26d9d4e1238405620cc9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-06-01 10:41:37 +0530

    Update BrokenLink_Practice.java

--- BrokenLink_Practice.java
+++ BrokenLink_Practice.java
@@ -4,9 +4,6 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
