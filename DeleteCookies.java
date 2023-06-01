7fcfe617f6b8593cd19d492eb711e2055d07e82a
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-06-01 10:41:56 +0530

    Update DeleteCookies.java

--- DeleteCookies.java
+++ DeleteCookies.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class DeleteCookies {
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
