7a5933de3ef94a4303fb63a58f3c48f70c30b673
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-06-01 10:42:54 +0530

    Update Proxy_plugins_DownloadpathsSeleniumjava.java

--- Proxy_plugins_DownloadpathsSeleniumjava.java
+++ Proxy_plugins_DownloadpathsSeleniumjava.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
 public class Proxy_plugins_DownloadpathsSeleniumjava {
 
 	public static void main(String[] args) {
