82587ce3963b7d84e90833a110cc638356480b36
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-01 11:11:23 +0530

    Update Screenshot.java

--- Screenshot.java
+++ Screenshot.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.IOException;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 
 
