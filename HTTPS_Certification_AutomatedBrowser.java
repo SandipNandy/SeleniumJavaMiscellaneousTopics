145272369a53a5b65addce75b3ac0462e17f5104
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-01 11:10:16 +0530

    Update HTTPS_Certification_AutomatedBrowser.java

--- HTTPS_Certification_AutomatedBrowser.java
+++ HTTPS_Certification_AutomatedBrowser.java
@@ -1,9 +1,12 @@
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
