7f3ac8e8f767143c0786abae1bdd0001071e027a
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-14 00:08:18 +0530

    Update Automation_Broken_Links.java

--- Automation_Broken_Links.java
+++ Automation_Broken_Links.java
@@ -7,7 +7,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import java.util.List;
-
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
