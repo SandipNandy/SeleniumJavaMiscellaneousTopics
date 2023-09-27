830132662504fbb856eb1fd0572cfa4b39b67e2e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-27 09:03:37 +0530

    Update Selenium4_Tabhandling_Screenshot_Weight_Height.java

--- Selenium4_Tabhandling_Screenshot_Weight_Height.java
+++ Selenium4_Tabhandling_Screenshot_Weight_Height.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
