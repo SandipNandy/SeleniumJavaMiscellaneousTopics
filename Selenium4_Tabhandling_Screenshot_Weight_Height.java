a60e57f492209c808cd21cb791e2240fd92b8793
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-27 09:05:40 +0530

    Update Selenium4_Tabhandling_Screenshot_Weight_Height.java

--- Selenium4_Tabhandling_Screenshot_Weight_Height.java
+++ Selenium4_Tabhandling_Screenshot_Weight_Height.java
@@ -16,7 +16,6 @@ public static void main(String[] args) throws IOException {
 // TODO Auto-generated method stub
 System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
 WebDriver driver=new ChromeDriver();
-
 driver.get("https://rahulshettyacademy.com/angularpractice/");
 //Switching Window
 
