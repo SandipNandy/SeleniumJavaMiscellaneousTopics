2870c248d2a4f111d84ce2729a87a8b094109c19
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-22 08:57:12 +0530

    Update BrokenLink_Practice.java

--- BrokenLink_Practice.java
+++ BrokenLink_Practice.java
@@ -16,6 +16,7 @@ public class BrokenLink_Practice {
 	public static void main(String[] args) throws MalformedURLException, IOException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
+		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://AutomationPractice/");
