1a8fb161e601fa421720e9504f546cab9822d270
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-31 08:52:43 +0530

    Update BrokenLink_Automation_SoftAssertion.java

--- BrokenLink_Automation_SoftAssertion.java
+++ BrokenLink_Automation_SoftAssertion.java
@@ -17,6 +17,7 @@ public class BrokenLink_Automation_SoftAssertion {
 		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		
 		WebDriver driver=new ChromeDriver();
+		
 		driver.manage().window().maximize();
 		driver.get("https://AutomationPractice/");
 		driver.manage().timeouts().implicitlyWait(6000, TimeUnit.SECONDS);
