5ce56c444663831e6b6c9f8f39261a2859ec3e2c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-06-11 23:25:42 +0530

    Update BrokenLink_Automation_SoftAssertion.java

--- BrokenLink_Automation_SoftAssertion.java
+++ BrokenLink_Automation_SoftAssertion.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.testng.asserts.SoftAssert;
 
 public class BrokenLink_Automation_SoftAssertion {
