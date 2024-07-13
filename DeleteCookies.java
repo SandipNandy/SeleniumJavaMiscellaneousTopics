7778581b3bdf6ab3d5478c7e23d6a6a0ea25c816
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-07-13 21:47:49 +0530

    Update DeleteCookies.java

--- DeleteCookies.java
+++ DeleteCookies.java
@@ -6,7 +6,6 @@ public class DeleteCookies {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		
 		driver.manage().window().maximize();
 		
 		driver.manage().deleteAllCookies();
