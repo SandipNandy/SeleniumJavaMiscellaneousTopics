7f31b7c6c6a8bd7c4846d26bcbfe3ba6fec7477d
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-07-20 09:56:18 +0530

    Update Automation_Broken_Links.java

--- Automation_Broken_Links.java
+++ Automation_Broken_Links.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Automation_Broken_Links {
 	
 	public static void main(String[] args) throws MalformedURLException, IOException {
+		
 		// TODO Auto-generated method stubs
 		// https://demoqa.com/links
 		System.setProperty("webdriver.chrome.driver", "..\\chromedriver_win32\\chromedriver.exe");
