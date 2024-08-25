085b00d95a74a8a3315cda404e7db302d14efcb6
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-08-25 14:25:46 +0530

    Update WebTable_SortingWith_SeleniumStreams.java

--- WebTable_SortingWith_SeleniumStreams.java
+++ WebTable_SortingWith_SeleniumStreams.java
@@ -18,7 +18,6 @@ driver.findElement(By.xpath("//tr/th[1]")).click();
 // capture all webelements into list
 List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
 // capture text of all webelements into new(original) list
-
 List<String> originalList = elementsList.stream().map(s -> s.getText()).collect(Collectors.toList());
 
 // sort on the original list of step 3 -> sorted list
