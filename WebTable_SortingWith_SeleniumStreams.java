53e89084d5d552d18378f1834206115c004b365d
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-08-31 08:04:23 +0530

    Update WebTable_SortingWith_SeleniumStreams.java

--- WebTable_SortingWith_SeleniumStreams.java
+++ WebTable_SortingWith_SeleniumStreams.java
@@ -18,6 +18,7 @@ driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
   
 // click on column
 driver.findElement(By.xpath("//tr/th[1]")).click();
+  
 // capture all webelements into list
 List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
 // capture text of all webelements into new(original) list
