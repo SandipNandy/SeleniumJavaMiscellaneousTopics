804dd62e84e96c8d183ba7c10caef5abf9d58437
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-06 11:03:30 +0530

    Update WebTable_SortingWith_SeleniumStreams.java

--- WebTable_SortingWith_SeleniumStreams.java
+++ WebTable_SortingWith_SeleniumStreams.java
@@ -11,7 +11,6 @@ public class WebTable_SortingWith_SeleniumStreams {
 public static void main(String[] args) throws InterruptedException {
 // TODO Auto-generated method stub
 System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-  
 WebDriver driver = new ChromeDriver();
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
   
