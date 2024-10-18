30270260a6741fa57655453dbff53f82a0a2c097
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-18 23:44:02 +0530

    Update DeleteCookies.java

--- DeleteCookies.java
+++ DeleteCookies.java
@@ -10,6 +10,7 @@ public class DeleteCookies {
 		WebDriver driver=new ChromeDriver();
 		
 		driver.manage().window().maximize();
+		
 		driver.manage().deleteAllCookies();
 		//If you want to delete the cookies name wise then use below code
 		//driver.manage().deleteCookieNamed("ASF");
