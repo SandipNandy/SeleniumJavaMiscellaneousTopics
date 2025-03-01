154588850fb37b053ab6a80b1dd8b9d67e3f3c39
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2025-03-01 11:13:17 +0530

    Update WebTable_SortingWith_SeleniumStreams.java

--- WebTable_SortingWith_SeleniumStreams.java
+++ WebTable_SortingWith_SeleniumStreams.java
@@ -19,6 +19,7 @@ public static void main(String[] args) throws InterruptedException {
 System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
   
 WebDriver driver = new ChromeDriver();
+  
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
   
 // click on column
