36b70510977cb2a0aba375f311febb0a2029067e
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-04 11:09:30 +0530

    Update FilterWebTablewithSeleniumJava.java

--- FilterWebTablewithSeleniumJava.java
+++ FilterWebTablewithSeleniumJava.java
@@ -3,7 +3,6 @@ import java.util.stream.Collectors;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
