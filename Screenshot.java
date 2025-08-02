760c915e9cc8392173e5f151460f03d237e9eab6
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-08-02 13:07:21 +0530

    Update Screenshot.java

--- Screenshot.java
+++ Screenshot.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.OutputType;
 
 import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.apache.commons.io.FileUtils;
@@ -25,6 +26,7 @@ public class Screenshot {
 	}
 
 }
+
 
 
 
