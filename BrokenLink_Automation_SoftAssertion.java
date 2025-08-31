dfd172e5cd76c3467c4303b1c620f5c5aeb7c643
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-08-31 11:27:05 +0530

    Update BrokenLink_Automation_SoftAssertion.java

--- BrokenLink_Automation_SoftAssertion.java
+++ BrokenLink_Automation_SoftAssertion.java
@@ -4,7 +4,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
@@ -51,6 +50,7 @@ public class BrokenLink_Automation_SoftAssertion {
 	}
 
 }
+
 
 
 
