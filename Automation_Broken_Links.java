f0a2a4ec0a1b22f0152584314c569bb085233e35
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-08-31 11:24:47 +0530

    Update Automation_Broken_Links.java

--- Automation_Broken_Links.java
+++ Automation_Broken_Links.java
@@ -17,7 +17,6 @@ public class Automation_Broken_Links {
 		
 		// https://demoqa.com/links
 		System.setProperty("webdriver.chrome.driver", "..\\chromedriver_win32\\chromedriver.exe");
-		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
@@ -81,6 +80,7 @@ public class Automation_Broken_Links {
 	}
 
 }
+
 
 
 
