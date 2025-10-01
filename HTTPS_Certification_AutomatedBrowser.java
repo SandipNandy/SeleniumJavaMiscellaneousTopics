3680cd867830ccecc1615c9389a4b77356a3ed9b
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-10-01 09:03:40 +0530

    Update HTTPS_Certification_AutomatedBrowser.java

--- HTTPS_Certification_AutomatedBrowser.java
+++ HTTPS_Certification_AutomatedBrowser.java
@@ -1,7 +1,6 @@
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.edge.EdgeDriver;
@@ -59,5 +58,6 @@ public class HTTPS_Certification_AutomatedBrowser {
 	}
 
 }
+
 
 
